ad33dc7824973fbdbe65582d7cc2193642213f93
Geir Haugom <haugom@gmail.com>
2022-11-30 11:04:40 +0100

    Make basic webserver

---
+++ internal/server.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func Start() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
